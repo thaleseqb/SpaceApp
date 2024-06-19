02c66f584c76fa42a2eb5b2af39aa6f4bf7b65ca
thales <thalesquadros2@gmail.com>
2024-06-19 10:38:15 -0300

    add input, but couldnt align input and the space-app logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
 import Header from "./components/header"
+import Input from "./TextFiled"
 
 const GradientBackground = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
+`
+
+const FStyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `
 
 function App() {
@@ -14,7 +21,11 @@ function App() {
     <GradientBackground>
       <GlobalStyles/>
 
-      <Header/>
+      <FStyledDiv>
+        <Header/>
+        <Input placeHolder="O que você deseja aprender" />
+      </FStyledDiv>
+      
     </GradientBackground>
   )
 }
--- src/TextFiled/index.jsx
+++ src/TextFiled/index.jsx
@@ -1,5 +1,41 @@
-import styled from "styled-components";
+import {styled} from "styled-components";
+import search from "./search.png"
+
+const StyledDiv = styled.div`
+    position: absolute;
+    display: inline-block;
+`
 
 const StyledInput = styled.input`
-    
+    height: 56px;
+    background: transparent;
+    border-color: #C98CF1;
+    border-radius: 10px;
+    box-sizing: border-box;
+    padding: 12px 16px;
+    width: 566px;
+    border: 2x solid;
+    color: #D9D9D9;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 20px;
 `
+
+const Icon = styled.img`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 38px;
+    height: 38px;
+`
+
+const Input = (props) => {
+    return (
+        <StyledDiv>
+            <StyledInput placeholder="O que você deseja aprender ?" {...props} />
+            <Icon src={search} alt="icone de lupa" />
+        </StyledDiv>
+    )
+}
+
+export default Input;
