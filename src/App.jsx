23e86ff674da80dbf4ba9cf93acc03fec9c05b90
thales <thalesquadros2@gmail.com>
2024-06-24 16:44:15 -0300

    complete the aside icons and add a background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
 import Header from "./components/header"
-import Input from "./TextFiled"
 import AsideBar from "./components/AsideBar"
+import Banner from "./components/Banner"
+// import Banner from "./components/Banner"
 
 const GradientBackground = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
+`
+
+const FStyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  margin-top: 20px;
 `
 
 function App() {
@@ -17,7 +25,15 @@ function App() {
       
       <GlobalStyles/>
       <Header/>
-      <AsideBar/>
+
+      <FStyledDiv>
+        <AsideBar/>
+        <Banner 
+          texto="A galeria mais completa de fotos do espaço!" 
+          backgroundImage="./imagens/banner.png"
+        />
+      </FStyledDiv>
+      
       
     </GradientBackground>
   )
--- src/components/AsideBar/index.jsx
+++ src/components/AsideBar/index.jsx
@@ -4,6 +4,7 @@ import NavItem from "./NavItem"
 const StyledList = styled.ul`
     list-style: none;
     margin: 0;
+    /* margin-left: -100px; */
     padding: 0;
     width: 212px;
 `
@@ -27,6 +28,24 @@ const AsideBar = () => {
                         inactiveIcon="/icons/mais-vistas-inativo.png"
                     >
                         Mais vistos
+                    </NavItem>
+                    <NavItem 
+                        activeIcon="/icons/mais-curtidas-ativo.png"
+                        inactiveIcon="/icons/mais-curtidas-inativo.png"
+                    >
+                        Mais curtidas
+                    </NavItem>
+                    <NavItem 
+                        activeIcon="/icons/novas.png"
+                        inactiveIcon="/icons/novas-inativo.png"
+                    >
+                        Novas
+                    </NavItem>
+                    <NavItem 
+                        activeIcon="/icons/surpreenda-me-ativo.png"
+                        inactiveIcon="/icons/surpreenda-me-inativo.png"
+                    >
+                        Surpreenda-me
                     </NavItem>
 
                 </StyledList>
